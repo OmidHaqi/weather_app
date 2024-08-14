030e6b80e487d55175f5fea8735dec9c40983557
Umut <omid.haqi@outlook.com>
2024-08-15 01:02:15 +0330

    create model and entitiy

---
+++ lib/features/feature_weather/domain/entities/current_city_entity.dart
@@ -0,0 +1,40 @@
+import 'package:equatable/equatable.dart';
+
+import '../../data/model/current_city_model.dart';
+
+class CurrentCityEntity extends Equatable {
+  final Coord? coord;
+  final List<Weather>? weather;
+  final String? base;
+  final Main? main;
+  final int? visibility;
+  final Wind? wind;
+  final Rain? rain;
+  final Clouds? clouds;
+  final int? dt;
+  final Sys? sys;
+  final int? timezone;
+  final int? id;
+  final String? name;
+  final int? cod;
+
+  const CurrentCityEntity({
+    this.coord,
+    this.weather,
+    this.base,
+    this.main,
+    this.visibility,
+    this.wind,
+    this.rain,
+    this.clouds,
+    this.dt,
+    this.sys,
+    this.timezone,
+    this.id,
+    this.name,
+    this.cod,
+  });
+
+  @override
+  List<Object?> get props => [];
+}
