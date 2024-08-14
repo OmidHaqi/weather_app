7756b126fbdac87cc9974d113eec44173114b687
Umut <omid.haqi@outlook.com>
2024-08-14 21:38:21 +0330

    init

---
+++ lib/core/widgets/main_wrapper.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MainWrapper extends StatelessWidget {
+  const MainWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('CleanArcitechtureEdu'),
+      ),
+    );
+  }
+}
