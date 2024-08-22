a6f7d58d85305011f16c07d90db6c12dab70c1b1
Umut <omid.haqi@outlook.com>
2024-08-22 12:56:54 +0330

    Reapply "refactor: :sparkles: hi"
    This reverts commit 391f963bda02523306a5965b849b4f41dcafa75a.

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,5 +1,4 @@
 class Constants {
   static const baseUrl = 'https://api.openweathermap.org';
-
-  static const apiKey1 = '4b478b6fb58c2384e2cf4a3e044a70d7'; // Add your API_KEY here
+  static const apiKey1 = 'YOUR_API_KEY'; // Add your API_KEY here
 }
--- lib/features/feature_weather/data/date_source/remote/api_provider.dart
+++ lib/features/feature_weather/data/date_source/remote/api_provider.dart
@@ -1,32 +1,32 @@
-import 'package:clean_arcitechture_edu/core/utils/constants.dart';
 import 'package:dio/dio.dart';
 
 import '../../../../../core/params/forecast_params.dart';
+import '../../../../../core/utils/constants.dart';
 
-//*reciver the data
+//*receiver the data
 
 class ApiProvider {
   final Dio _dio = Dio();
-  var apikey = Constants.apiKey1;
+  var apiKey = Constants.apiKey1;
 
   //? current weather api call
   Future<dynamic> callCurrentWeather(cityName) async {
     var response = await _dio
         .get('${Constants.baseUrl}/data/2.5/weather', queryParameters: {
       'q': cityName,
-      'appid': apikey,
+      'appid': apiKey,
       'units': 'metric',
     });
     return response;
   }
 
   //? 5 days forecast api
-  Future<dynamic> sendRequest5DaysForcast(ForecastParams params) async {
+  Future<dynamic> sendRequest5DaysForecast(ForecastParams params) async {
     var response = await _dio.get('${Constants.baseUrl}/data/2.5/forecast',
         queryParameters: {
           'lat': params.lat,
           'lon': params.lon,
-          'appid': apikey,
+          'appid': apiKey,
           'units': 'metric'
         });
     return response;
